764b8835726a8b786b4fa322408ff37b56a0693e
birdkyle7918 <bbirdkkyle@gmail.com>
2021-08-15 10:14:42 +0800

    horizon and vertical in every move

--- src/index.js
+++ src/index.js
@@ -57,8 +57,12 @@ class Game extends React.Component {
 
             // squares 是一个长度为 9 的数组，保存了 9 个格子的状态
             // history 是一个不断增加内容的数组，保存了每一步的历史记录
+            // horizon 是落子的横坐标
+            // vertical 是落子的纵坐标
             history: [{
                 squares: Array(9).fill(null),
+                horizon: null,
+                vertical: null,
             }],
 
             // 判断下一个落子者是否是 'X'
@@ -70,7 +74,7 @@ class Game extends React.Component {
     }
 
     /*
-    * 点击某个方格，触发的动作
+    * 点击某个方格，触发的动作，参数只有索引值 i，意思是把 squares 的第 i 个元素上落下棋子。
     * */
     handleClick(i){
 
@@ -78,23 +82,27 @@ class Game extends React.Component {
         // 这样表示 history 可以保证我们把这些“未来”的不正确的历史记录丢弃掉。
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
 
-        // 目前的记录，是 history 数组中的最后一条（由于“未来”的不正确的历史记录已经被丢弃）
+        // 先获取目前历史记录中的最后一条
         const current = history[history.length - 1];
-
-        // squares 是根据 current 确定
         const squares = current.squares.slice();
+
+        // 落子横坐标确定
+        const horizon = i % 3;
+
+        // 落子纵坐标确定
+        const vertical = parseInt(i / 3);
 
         // 如果已经决出胜者了，那么就不做事了，直接返回
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
 
-        // 根据 xIsNext 来判断是画 X，还是画 O
+        // 在历史记录的最后一条的基础上改动，根据 xIsNext 来判断是画 X，还是画 O
         squares[i] = this.state.xIsNext ? 'X' : 'O';
 
         // 添加历史记录，并且把 xIsNext 置反，并且把步数记录为 history 数组的长度
         this.setState({
-            history: history.concat([{squares: squares}]),
+            history: history.concat([{squares: squares, horizon: horizon, vertical: vertical}]),
             xIsNext: !this.state.xIsNext,
             stepNumber: history.length,
         });
@@ -112,14 +120,21 @@ class Game extends React.Component {
 
     render() {
 
-        // 渲染到最终的页面时，hostory 就是目前目前的；current 是按照 stepNumber 来索引；winner 是根据 current 来计算。
+        // 渲染到最终的页面时，hostory 就是目前的；current 是按照 stepNumber 来索引；winner 是根据 current 来计算。
         const history = this.state.history;
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         // 把历史步骤映射为代表按钮的 React 元素，然后可以展示出一个按钮的列表，点击这些按钮，可以“跳转”到对应的历史步骤
         const moves = history.map((step, move) => {
-            const buttonDescribe = move ?'Go to move ' + move : 'Go to game start';
+
+            // 每一次落子的横纵坐标以及落子角色（叉叉或圆圈）
+            const oneMoveHorizon = history[move].horizon;
+            const oneMoveVertical = history[move].vertical;
+
+            const buttonDescribe = move ?
+                'Go to move ' + move + ' (' + oneMoveHorizon + ',' + oneMoveVertical + ') '
+                : 'Go to game start';
 
             // 返回一个 button 列表，可以回到任意一个历史时刻
             return (
